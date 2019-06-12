ef6d3683ff7d60fe820483b60f3b233484772046
gatesc <charles.gates+git@gmail.com>
2019-06-11 23:24:51 -0400

    force the xml2json to return an object instead of a string

--- server/bbg.js
+++ server/bbg.js
@@ -9,15 +9,17 @@ module.exports = {
     let searchUrl = "https://www.boardgamegeek.com/xmlapi2/search?exact=1&type=boardgame&query="+n;
     console.log(searchUrl)
     var xmlInfo = await axios.get(searchUrl);
-    var info = JSON.parse(xmlParser.toJson(xmlInfo.data));
-    console.log(info.items)
+//    var info = JSON.parse(xmlParser.toJson(xmlInfo.data));
+    var info = xmlParser.toJson(xmlInfo.data, {object: true, trim: true, sanitize: true});
+    console.log(JSON.stringify(info.items))
 
     let lookupUrl = "https://www.boardgamegeek.com/xmlapi2/thing?id="+info.items.item.id;
     console.log(lookupUrl);
     xmlInfo = await axios.get(lookupUrl);
     console.log(typeof(xmlInfo.data))
-    info = xmlParser.toJson(xmlInfo.data);
-    console.log(info);
+    //info = JSON.parse(xmlParser.toJson(xmlInfo.data));
+    info = xmlParser.toJson(xmlInfo.data, {object: true, trim: true, sanitize: true})
+    console.log(JSON.stringify(info));
     return info;
   }
 
